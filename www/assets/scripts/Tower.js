71922f5188b57f15a092037629614105586d6066
LuxARTS <bacelolucas@gmail.com>
2019-12-03 18:11:09 -0300

    Tower shot

--- www/assets/scripts/Tower.js
+++ www/assets/scripts/Tower.js
@@ -16,7 +16,7 @@ class Tower{
         
         if(config)Object.assign(this.options, this.options, config);
         this.options.spacing = parseFloat((this.options.cellSize/10).toFixed(1));
-
+        
         this.draw();
     }
     draw(){
@@ -76,5 +76,50 @@ class Tower{
         ctx.fillStyle = this.player? this.options.colorP2 : this.options.colorP1;
         ctx.fill();
         ctx.restore();
+    }
+    shot(length){
+        const el = this.options.element;
+        let ctx = el.getContext('2d');
+        const posX = this.position[0]*this.options.cellSize;
+        const posY = this.position[1]*this.options.cellSize;
+        const cell = this.options.cellSize;
+        
+        ctx.save();
+        ctx.beginPath();
+        switch(this.rotation){
+            case 0:
+                break;
+            case 1:
+                // Move center
+                ctx.translate(posX+cell/2, posY+cell/2);
+                // Rotate
+                ctx.rotate(90*Math.PI/180);
+                // Move origin back
+                ctx.translate(-posX-cell/2, -posY-cell/2);
+                break;
+            case 2:
+                // Move center
+                ctx.translate(posX+cell/2, posY+cell/2);
+                // Rotate
+                ctx.rotate(180*Math.PI/180);
+                // Move origin back
+                ctx.translate(-posX-cell/2, -posY-cell/2);
+                break;
+            case 3:
+                // Move center
+                ctx.translate(posX+cell/2, posY+cell/2);
+                // Rotate
+                ctx.rotate(270*Math.PI/180);
+                // Move origin back
+                ctx.translate(-posX-cell/2, -posY-cell/2);
+                break;            
+        }
+        ctx.rect(posX+cell/2-cell/16, posY, cell/8, -cell*length);
+        ctx.restore();
+        ctx.fillStyle = "#FF0000";
+        ctx.fill();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "#000000";
+        ctx.stroke();
     }
 }
